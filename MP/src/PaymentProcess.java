eb4905ca59f031955b80229c4d441f046a634003
KennyTeear <kendrick_chan@dlsu.edu.ph>
2023-06-25 17:34:07 +0800

    yes

--- MP/src/PaymentProcess.java
+++ MP/src/PaymentProcess.java
@@ -1,2 +1,5 @@
 public class PaymentProcess {
+    private int balance;
+
+    public PaymentProcess
 }
--- MP/src/VendingMachineFactory.java
+++ MP/src/VendingMachineFactory.java
@@ -1,2 +1,25 @@
 public class VendingMachineFactory {
+    private VendingMachine vendingMachine;
+
+    public VendingMachineFactory() {
+        this.vendingMachine = null;
+    }
+
+    public VendingMachine createVendingMachine() {
+        this.vendingMachine = new VendingMachine();
+        return this.vendingMachine;
+    }
+
+    public void testVendingMachine() {
+        if (this.vendingMachine != null) {
+            // the testing part?
+        } else {
+            System.out.println("No vending machine created yet.");
+        }
+    }
+
+    public void exitProgram() {
+        System.out.println("Exiting program.");
+        System.exit(0);
+    }
 }
