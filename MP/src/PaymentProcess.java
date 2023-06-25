65679c20cdaef82f8324587fdb7b8daa86119b93
KennyTeear <kendrick_chan@dlsu.edu.ph>
2023-06-25 17:53:16 +0800

    added more stuff

--- MP/src/PaymentProcess.java
+++ MP/src/PaymentProcess.java
@@ -1,5 +1,22 @@
 public class PaymentProcess {
     private int balance;
 
-    public PaymentProcess
+    public PaymentProcess(int balance){
+        this.balance = balance;
+    }
+
+    public void receivePayment(int payment){
+
+    }
+
+    public void giveChange(int change){
+
+    }
+
+    public void replenishChange(int change){
+
+    }
+    public int collectPayment() {
+        balance = 0;
+    }
 }
--- MP/src/VendingMachine.java
+++ MP/src/VendingMachine.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+
 public class VendingMachine {
+    private Slots[] slots;
+    private ArrayList<Transaction> transactionLog;
+    private PaymentProcess paymentProcess;
+
+
+    public VendingMachine(Slots[] slots, PaymentProcess paymentProcess){
+        this.slots = slots;
+        this.paymentProcess = paymentProcess;
+    }
+
+    public void displayItems(){
+
+    }
+
+    public void receivePayment(int payment){
+
+    }
+
+    public Item dispenseItem(int slotNumber){
+
+    }
+
+    public void produceChange(int amount){
+
+    }
+
+    public void restockItem(int slotNumber, int qty){
+
+    }
+
+    public void setPrice(int slotNumber, int qty){
+
+    }
+
+    public int collectPayment(){
+
+    }
+
+    public void replenishChange(int amount){
+
+    }
+
+    public void printTransactionSummary(){
+
+    }
+
+
+
 }
